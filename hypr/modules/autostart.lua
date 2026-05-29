05a76c4dec412817df52a28db982c85ac1c7ccaa
juicygoose007 <jaketurner624@gmail.com>
2026-05-29 07:10:52 -0400

    focus DP-2 on startup

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -23,4 +23,8 @@ hl.on("hyprland.start", function()
 
 	-- Normal start --
 	hl.exec_cmd("hymission")
+
+	-- Focus Main workspace via DP-2
+	hl.exec_cmd("hyprctl dispatch focusmonitor DP-2")
+	hl.exec_cmd("hyprctl dispatch focuswindow class:^com\\.mitchellh\\.ghostty$")
 end)
