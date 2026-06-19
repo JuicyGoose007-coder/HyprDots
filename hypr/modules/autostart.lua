e5145220b5d7e9c9a39f4b0ab73198d8ea3b2e05
juicygoose007 <jaketurner624@gmail.com>
2026-06-18 21:14:34 -0400

    waybar autostart reworked

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -6,13 +6,15 @@
 hl.on("hyprland.start", function()
 	hl.exec_cmd("uwsm app -s b -t service -- /usr/lib/hyprpolkitagent/hyprpolkitagent")
 	hl.exec_cmd("uwsm app -s b -t service -- awww-daemon")
-	hl.exec_cmd("uwsm app -s b -t service -- ~/.config/waybar/waybar.sh")
+	hl.exec_cmd("uwsm app -s b -t service -- waybar")
+	-- hl.exec_cmd("uwsm app -s b -t service -- ~/.config/waybar/waybar.sh")
 	hl.exec_cmd("uwsm app -s b -t service -- swaync")
 	hl.exec_cmd("uwsm app -s b -t service -- hypridle")
 	hl.exec_cmd("uwsm app -s b -t service -- wl-paste --watch cliphist store")
 	hl.exec_cmd("uwsm app -s b -t service -- wl-paste --type image --watch cliphist store")
 
 	-- Workspaces --
+
 	-- Discord --
 	hl.exec_cmd("uwsm app -s b -t service -- vesktop")
 	-- hl.exec_cmd("sleep 2 && uwsm app -s b -t service -- ghostty --title=riptide -e zsh -ic riptide")
@@ -22,7 +24,7 @@ hl.on("hyprland.start", function()
 	-- hl.exec_cmd("uwsm app -- zen-browser", { workspace = "name:Main" })
 
 	-- Normal start --
-	-- hl.exec_cmd("hymission")
+	hl.exec_cmd("hymission")
 
 	-- Focus Ghostty on Main workspace via DP-2
 	hl.exec_cmd("hyprctl dispatch focusmonitor DP-2")
